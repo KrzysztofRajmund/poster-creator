474e8e5dceb1e457daa34a780707df14906c029e
KrzysztofRajmund <info.k2nstudio@gmail.com>
2025-02-04 21:26:05 +0100

    PC-04022025_2 Modified ActionCard

--- src/components/ActionCard/ActionCard.tsx
+++ src/components/ActionCard/ActionCard.tsx
@@ -11,15 +11,15 @@ export const ActionCard = ({ icon, text, onClick }: ActionCardProps) => {
     <div
       role="button"
       tabIndex={0}
-      className="relative flex h-[256px] w-[365px] cursor-pointer flex-col items-center justify-center gap-0 rounded-[10px] bg-white-97 transition-colors duration-300 hover:bg-black-25 focus:outline-none focus:ring-4 focus:ring-primary focus:ring-opacity-50"
+      className="relative flex h-[256px] max-w-[365px] cursor-pointer flex-col items-center justify-center gap-0 rounded-[10px] bg-white-97 transition-colors duration-300 hover:bg-black-25 focus:outline-none focus:ring-4 focus:ring-primary focus:ring-opacity-50"
       onClick={onClick}
     >
-      <div className="relative flex h-full max-h-[96px] w-full flex-grow items-center justify-center">
+      <div className="flex max-h-[96px] flex-grow items-center justify-center">
         {icon}
       </div>
-      <div className="absolute bottom-0 w-full pb-[12px] text-center text-black-100">
+      <span className="absolute bottom-0 pb-[12px] text-center text-md">
         {text}
-      </div>
+      </span>
     </div>
   );
 };
