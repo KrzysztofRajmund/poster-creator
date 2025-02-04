05aef94fb2de106430dbb83e86dde4bfb0e2ab3c
KrzysztofRajmund <info.k2nstudio@gmail.com>
2025-02-04 21:26:57 +0100

    PC-04022025_2 Created PosterActions

---
+++ src/components/PosterActions/PosterActions.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+
+import { ActionCard } from "../ActionCard";
+
+import BackgroundIcon from "@/assets/icons/bg-icon.svg";
+import ImagePlaceholderIcon from "@/assets/icons/img-placeholder.svg";
+import TextIcon from "@/assets/icons/text.svg";
+
+interface PosterActionsProps {
+  addImageElement: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  addTextElement: () => void;
+  handleBackgroundChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const PosterActions = ({
+  addImageElement,
+  addTextElement,
+  handleBackgroundChange,
+}: PosterActionsProps) => {
+  return (
+    <div className="mb-auto grid grid-cols-1 justify-center gap-x-[29px] gap-y-8 2xl:grid-cols-2 2xl:justify-start">
+      <h4 className="col-span-full rounded-[10px] bg-white-97 px-4 py-6 text-md font-bold">
+        Add content
+      </h4>
+      <ActionCard
+        icon={<TextIcon width={96} height={96} />}
+        text="Add text"
+        onClick={addTextElement}
+      />
+      <ActionCard
+        icon={<ImagePlaceholderIcon width={96} height={88} />}
+        text="Add image"
+        onClick={() => document.getElementById("imageInput")?.click()}
+      />
+      <input
+        className="hidden"
+        id="imageInput"
+        type="file"
+        accept="image/*"
+        onChange={addImageElement}
+      />
+      <ActionCard
+        icon={<BackgroundIcon width={96} height={96} />}
+        text="Add background image"
+        onClick={() => document.getElementById("backgroundInput")?.click()}
+      />
+      <input
+        className="hidden"
+        id="backgroundInput"
+        type="file"
+        accept="image/*"
+        onChange={handleBackgroundChange}
+      />
+    </div>
+  );
+};
